bc4b4117723cc4b6c36446b38d8462c29c5dc244
swampcoin <52452391+swampcoin@users.noreply.github.com>
2019-07-27 15:34:51 -0400

    Update swamp.cpp

--- src/qt/swamp.cpp
+++ src/qt/swamp.cpp
@@ -569,7 +569,7 @@ int main(int argc, char *argv[])
     QTextCodec::setCodecForCStrings(QTextCodec::codecForTr());
 #endif
 
-    Q_INIT_RESOURCE(swamp);
+  // Q_INIT_RESOURCE(swamp);
     Q_INIT_RESOURCE(swamp_locale);
 
     BitcoinApplication app(argc, argv);
